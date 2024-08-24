3f0d5b22fdd2885aec9b25d2a031d827eebabe7c
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 14:07:31 +0200

    update yaml-each: add argument parsing and version display

--- spec/yaml_each_spec.rb
+++ spec/yaml_each_spec.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "tempfile"
 
-RSpec.describe "each" do
+RSpec.describe "yaml-each" do
+  it "works" do
+    file = Tempfile.create
+    File.write(file, <<~INPUT)
+      - 1
+      - 2
+      - 3
+    INPUT
+    expect(`bin/yaml-each #{file.path} "p it"`).to eq(<<~OUTPUT)
+      1
+      2
+      3
+    OUTPUT
+  end
 end
